c7415e8b9dc4f1f6d83fded931f82216758838c9
Huseyin BABAL <huseyinbabal88@gmail.com>
2016-03-28 09:39:52 +0300

    Price formatting phase-2 - Price formatting implementation finished - Required unit tests are added

--- test/unit/UtilsTest.js
+++ test/unit/UtilsTest.js
@@ -4,17 +4,78 @@ var assert = require('assert'),
 
 describe('Iyzipay', function () {
 
-    it('should convert integer price', function (done) {
-        var price = utils.asPrice('1');
-        console.log(price);
-        price.should.be.equal('1.0');
+    var iyziWsHeaderName;
+    var apiKey;
+    var secretKey;
+    var separator;
+    var randomString;
+    var body;
+
+    before(function() {
+        iyziWsHeaderName = "IYZWS";
+        apiKey = "api_key";
+        secretKey = "secret_key";
+        separator = ":";
+        randomString = "random_string";
+        body = "body";
+    });
+
+    it('should return api methods', function(done) {
+        var apiMethods = {
+            RETRIEVE: 'retrieve',
+            CREATE: 'create',
+            DELETE: 'delete',
+            UPDATE: 'update'
+        };
+        should.deepEqual(apiMethods, utils.apiMethod);
+        done();
+    });
+
+    it('should generate authorization header', function(done) {
+        var header = utils.generateAuthorizationHeader(iyziWsHeaderName, apiKey, separator, secretKey, body, randomString);
+        header.should.be.equal("IYZWS api_key:ikF+xhjLA0/xsvl+eJjoHWkwh5g=");
+        done();
+    });
+
+    it('should generate hash', function(done) {
+        var hash = utils.generateHash(apiKey, randomString, secretKey, body);
+        hash.should.be.equal("ikF+xhjLA0/xsvl+eJjoHWkwh5g=");
+        done();
+    });
+
+    it('should generate request string for basic request', function(done) {
+        var requestString = utils.generateRequestString({name: "John", surname: "Doe"});
+        requestString.should.be.equal("[name=John,surname=Doe]");
+        done();
+    });
+
+    it('should generate request string for request that has array values', function(done) {
+        var requestString = utils.generateRequestString({name: "John", surname: "Doe", friends: [{name: "Johnny"}, {name: "Mike"}]});
+        requestString.should.be.equal("[name=John,surname=Doe,friends=[[name=Johnny], [name=Mike]]]");
+        done();
+    });
+
+    it('should generate random string', function(done) {
+        var randomString = utils.generateRandomString(8);
+        randomString.should.not.be.null;
+        done();
+    });
+
+    it('should convert non-float price', function (done) {
+        var price = utils.formatPrice('22');
+        price.should.be.equal('22.0');
         done();
     });
 
     it('should convert float price', function (done) {
-        var price = utils.asPrice('1.0');
-        console.log(price);
-        price.should.be.equal('1.0');
+        var price = utils.formatPrice('23.0');
+        price.should.be.equal('23.0');
+        done();
+    });
+
+    it('should eliminate floating zeros', function (done) {
+        var price = utils.formatPrice('15.340000');
+        price.should.be.equal('15.34');
         done();
     });
 });
