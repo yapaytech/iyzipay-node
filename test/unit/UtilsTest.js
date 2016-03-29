f0659936898771a4b13ee0fe94eb3bfb31cf0f3b
Sabri Onur Tüzün <onur.tuzun@iyzico.com>
2016-03-29 12:47:17 +0300

    unit tests updated and format price method added

--- test/unit/UtilsTest.js
+++ test/unit/UtilsTest.js
@@ -11,7 +11,7 @@ describe('Iyzipay', function () {
     var randomString;
     var body;
 
-    before(function() {
+    before(function () {
         iyziWsHeaderName = "IYZWS";
         apiKey = "api_key";
         secretKey = "secret_key";
@@ -20,7 +20,7 @@ describe('Iyzipay', function () {
         body = "body";
     });
 
-    it('should return api methods', function(done) {
+    it('should return api methods', function (done) {
         var apiMethods = {
             RETRIEVE: 'retrieve',
             CREATE: 'create',
@@ -31,31 +31,38 @@ describe('Iyzipay', function () {
         done();
     });
 
-    it('should generate authorization header', function(done) {
+    it('should generate authorization header', function (done) {
         var header = utils.generateAuthorizationHeader(iyziWsHeaderName, apiKey, separator, secretKey, body, randomString);
         header.should.be.equal("IYZWS api_key:ikF+xhjLA0/xsvl+eJjoHWkwh5g=");
         done();
     });
 
-    it('should generate hash', function(done) {
+    it('should generate hash', function (done) {
         var hash = utils.generateHash(apiKey, randomString, secretKey, body);
         hash.should.be.equal("ikF+xhjLA0/xsvl+eJjoHWkwh5g=");
         done();
     });
 
-    it('should generate request string for basic request', function(done) {
-        var requestString = utils.generateRequestString({name: "John", surname: "Doe"});
+    it('should generate request string for basic request', function (done) {
+        var requestString = utils.generateRequestString({
+            name: "John",
+            surname: "Doe"
+        });
         requestString.should.be.equal("[name=John,surname=Doe]");
         done();
     });
 
-    it('should generate request string for request that has array values', function(done) {
-        var requestString = utils.generateRequestString({name: "John", surname: "Doe", friends: [{name: "Johnny"}, {name: "Mike"}]});
+    it('should generate request string for request that has array values', function (done) {
+        var requestString = utils.generateRequestString({
+            name: "John",
+            surname: "Doe",
+            friends: [{name: "Johnny"}, {name: "Mike"}]
+        });
         requestString.should.be.equal("[name=John,surname=Doe,friends=[[name=Johnny], [name=Mike]]]");
         done();
     });
 
-    it('should generate random string', function(done) {
+    it('should generate random string', function (done) {
         var randomString = utils.generateRandomString(8);
         randomString.should.not.be.null;
         done();
@@ -76,6 +83,18 @@ describe('Iyzipay', function () {
     it('should eliminate floating zeros', function (done) {
         var price = utils.formatPrice('15.340000');
         price.should.be.equal('15.34');
+        done();
+    });
+
+    it('should not eliminate leading zero', function (done) {
+        var price = utils.formatPrice('0.12345');
+        price.should.be.equal('0.12345');
+        done();
+    });
+
+    it('should not eliminate zeros in floating value', function (done) {
+        var price = utils.formatPrice('23.00450067');
+        price.should.be.equal('23.00450067');
         done();
     });
 });
